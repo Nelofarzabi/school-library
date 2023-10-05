afd2b8c8958893aca0fbd76c1f5598f63ce7c2e0
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-10-05 10:59:33 +0430

    Update teacher file

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(age, specialization, name = 'Uknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
