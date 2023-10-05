4879057f89c13ea6c2a83ad74251ee29c6fe58d2
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-10-05 10:58:32 +0430

    Update student file

--- student.rb
+++ student.rb
@@ -1,10 +1,12 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Uknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.students.push(self)
   end
 
   def play_hook
